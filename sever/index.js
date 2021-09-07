2998123ad9d61502853b6837988f094f2c1f2eb7
msunoosy <m.sunoos@gmail.com>
2021-09-07 16:30:13 +0530

    Mongoose connected

--- sever/index.js
+++ sever/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
 app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://user:root@cluster0.trhmt.mongodb.net/food?retryWrites=true&w=majority",
+  {
+    useNewUrlparser: true,
+  }
+);
 
 app.listen(3001, () => {
   console.log("Server is running on 3001");
