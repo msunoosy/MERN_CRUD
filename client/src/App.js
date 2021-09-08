e71c46b6e793356172a688b4a4e1ccb3c3baa4be
msunoosy <m.sunoos@gmail.com>
2021-09-08 16:40:29 +0530

    input form created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [FoodName, setFoodName] = useState("");
+  const [days, setdays] = useState(0);
+
+  const listHandler = () => {
+    console.log(FoodName + days);
+  };
+
   return (
-    <div>
-      <h1>sunoos</h1>
+    <div className="App">
+      <label>Food name</label>
+      <input
+        onChange={(e) => {
+          setFoodName(e.target.value);
+        }}
+      ></input>
+      <label>Days</label>
+      <input
+        onChange={(e) => {
+          setdays(e.target.value);
+        }}
+      ></input>
+      <button onClick={listHandler}>Submit</button>
     </div>
   );
 }
